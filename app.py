4a53d9fe70536809f225192b24cd86efd2a7bfb3
jili4976 <jili4976@uni.sydney.edu.au>
2024-04-15 12:18:40 +1000

    socket test on app.py

--- app.py
+++ app.py
@@ -85,5 +85,5 @@ def home():
 
 
 if __name__ == '__main__':
-    socketio.run(app)
-    #socketio.run(app,ssl_context=('certs/jili4976.tsm.crt', 'certs/jili4976.tsm.key'))
+    #socketio.run(app)
+    socketio.run(app,ssl_context=('certs/jili4976.tsm.crt', 'certs/jili4976.tsm.key'))
